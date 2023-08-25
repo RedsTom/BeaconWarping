9a6d07dcdb039d468621e32a00d918d5068c1bc4
Tom BUTIN <tom.frefre.13@gmail.com>
2023-08-25 22:57:39 +0200

    Added a class to get a random name from the previously commited names.txt file

---
+++ src/main/java/me/redstom/beaconwarp/common/RandomNameGenerator.java
@@ -0,0 +1,32 @@
+package me.redstom.beaconwarp.common;
+
+import com.google.inject.Singleton;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.stream.Stream;
+
+@Singleton
+public class RandomNameGenerator {
+
+    private static final Random RANDOM = new Random();
+
+    private final String[] names;
+
+    public RandomNameGenerator() {
+        InputStream file = getClass().getClassLoader()
+                                     .getResourceAsStream("names.txt");
+        BufferedReader bis = new BufferedReader(new InputStreamReader(file));
+        this.names = bis.lines()
+                        .toArray(String[]::new);
+    }
+
+    public String pickOne() {
+        return Stream.of(names)
+                     .skip(RANDOM.nextInt(names.length))
+                     .findFirst()
+                     .orElseThrow();
+    }
+}
